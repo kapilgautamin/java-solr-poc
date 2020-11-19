a2f87bb48fa7eea7725873925d28a5f444813230
Kapil Gautam <kapil.gautam@utdallas.edu>
2020-11-19 17:37:56 -0600

    Initial commit from spring intializer

---
+++ src/test/java/com/karmaniti/javasolrpoc/JavaSolrPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karmaniti.javasolrpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSolrPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
