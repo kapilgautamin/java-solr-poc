389f19789d938f59d54237c42456debed0e403f7
Kapil Gautam <kapil.gautam@utdallas.edu>
2020-11-20 21:43:57 -0600

    Remove unused imports

--- solr-poc/src/main/java/com/solr/poc/controllers/HomeController.java
+++ solr-poc/src/main/java/com/solr/poc/controllers/HomeController.java
@@ -1,19 +1,14 @@
 package com.solr.poc.controllers;
 
-import com.solr.poc.models.SearchInput;
 import com.solr.poc.services.CustomersService;
 
-import org.apache.tomcat.util.http.parser.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.solr.core.RequestMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -30,7 +25,7 @@ public class HomeController {
    @PostMapping(value = "/results")   
    public String indexSubmit(@RequestParam String content, Model model) {
       // System.out.println("Posted is "+ content);
-      model.addAttribute("output", _customersService.findByNamedQuery(content, PageRequest.of(0, 5)));
+      model.addAttribute("output", _customersService.findByNamedQuery(content, PageRequest.of(0, DEFAULT_PAGE_SIZE)));
       return "results";
    }
 }
--- solr-poc/src/main/java/com/solr/poc/controllers/SearchController.java
+++ solr-poc/src/main/java/com/solr/poc/controllers/SearchController.java
@@ -1,35 +1,19 @@
 package com.solr.poc.controllers;
 
-import com.solr.poc.models.SearchInput;
 import com.solr.poc.services.CustomersService;
 import com.solr.poc.entities.Customers;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class SearchController {
 
    @Autowired
    private CustomersService _customersService;
-
-
 
    @RequestMapping(value = "/search/{term}")
    public Page<Customers> search(@PathVariable String term) {
